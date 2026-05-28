8c7b5f21214f45dccc07ba0812d74cb8e5faac96
BOCO SALOMON <boco0075620@univmetiers.ci>
2026-05-28 10:59:15 +0000

    Méthode Add implémentée

--- src/BookShelfSpec.java
+++ src/BookShelfSpec.java
@@ -1,20 +1,28 @@
+import bookstoread.BookShelf;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
     public class BookShelfSpec {
         @Test
         public void shelfEmptyWhenNoBookAdded() throws Exception {
-            var Bookshelf = new bookstoread.BookShelf();
-            List<String> books = shelf.books();
+            BookShelf Shelf= new BookShelf();
+            List<String> books = Shelf.books();
             assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
         }
+
+        @Test
+        void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+            bookstoread.BookShelf shelf = new bookstoread.BookShelf();
+            shelf.add("Effective Java");
+            shelf.add("Code Complete");
+            List<String> books = shelf.books();
+            assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+        }
     }
-}
+
