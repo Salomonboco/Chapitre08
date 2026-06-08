c60367cb3468283da8846dfee991e1cfbd43f06e
S4-POSTE273 <salomonboco12@mail.com>
2026-06-08 10:50:48 +0000

    Succès de tous les tests après l'implémentation.

--- src/BookShelfSpec.java
+++ src/BookShelfSpec.java
@@ -1,14 +1,16 @@
 import bookstoread.Book;
 import bookstoread.BookShelf;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.time.Year;
+import java.util.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookShelfSpec {
@@ -16,6 +18,8 @@ public class BookShelfSpec {
     private Book effectiveJava;
     private Book codeComplete;
     private Book mythicalManMonth;
+    private Book cleanCode;
+
 
     @BeforeEach
     void init() throws Exception {
@@ -23,6 +27,7 @@ public class BookShelfSpec {
         effectiveJava = new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8));
         codeComplete = new Book("Code Complete", "Steve McConnel", LocalDate.of(2004, Month.JUNE, 9));
         mythicalManMonth = new Book("The Mythical Man-Month", "Frederick Phillips Brooks", LocalDate.of(1975, Month.JANUARY, 1));
+        cleanCode = new Book("Clean Code", "Robert C. Martin", LocalDate.of(2008, Month.AUGUST, 1)); // ← ajoute cette ligne
     }
 
     @Test
@@ -78,4 +83,15 @@ public class BookShelfSpec {
         List<Book> books = shelf.arrange(Comparator.<Book>naturalOrder().reversed());
         assertEquals(asList(mythicalManMonth, effectiveJava, codeComplete), books, () -> "Books in a bookshelf are arranged in descending order of book title");
     }
+
+   @Test
+    @DisplayName("books inside bookshelf are grouped by publication year")
+    void groupBooksInsideBookShelfByPublicationYear() {
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth, cleanCode);
+        Map<Year, List<Book>> booksByPublicationYear = shelf.groupByPublicationYear();
+        assertThat(booksByPublicationYear).containsKey(Year.of(2008)).containsValues(Arrays.asList(effectiveJava, cleanCode));
+        assertThat(booksByPublicationYear).containsKey(Year.of(2004)).containsValues(Collections.singletonList(codeComplete));
+        assertThat(booksByPublicationYear).containsKey(Year.of(1975)).containsValues(Collections.singletonList(mythicalManMonth));
+    }
+
 }
--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,4 +1,6 @@
 package bookstoread;
+import java.time.LocalDate;
+import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,4 +21,11 @@ public class BookShelf {
     public List<Book> arrange(Comparator<Book> criteria) {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return  books.stream().collect(Collectors.groupingBy(book ->
+                Year.of(book.getPublishedOn().getYear())));
+    }
+
+
 }
