23aeaa69723381a0fba2d74409108e3beb568722
S4-POSTE273 <salomonboco12@mail.com>
2026-06-08 10:54:31 +0000

    Tous les tests réussissent après cette l'implémentation

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -2,6 +2,7 @@ package bookstoread;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BookShelf {
@@ -27,5 +28,7 @@ public class BookShelf {
                 Year.of(book.getPublishedOn().getYear())));
     }
 
-
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books.stream().collect(Collectors.groupingBy(fx));
+    }
 }
